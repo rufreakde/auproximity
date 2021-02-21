0c315c4dd76d0bdc8d866b063701f6b644432363
Mehmet <zelakri@web.de>
2021-02-21 22:18:04 +0100

    Dialog vom Serverconnector

--- auproximity-webui/src/components/HostOptions.vue
+++ auproximity-webui/src/components/HostOptions.vue
@@ -1,6 +1,17 @@
 <template>
   <v-card class="pa-5">
     <h3>Host options</h3>
+    <v-dialog
+      v-model="dialog"
+      max-width="290"
+    >
+      <v-card>
+        <v-card-title class="headline">
+          Use Google's location service?
+        </v-card-title>
+        <ServerConnector @joinroom="joinRoom($event)" />
+      </v-card>
+    </v-dialog>
     <v-form>
       <v-slider
         label="Falloff"
@@ -42,6 +53,13 @@
         :disabled="!$store.state.joinedRoom"
         @change="updateOptions"
       ></v-checkbox>
+      <v-btn
+          color="success"
+          class="mr-4"
+          @click="dialog = !dialog"
+        >
+          Update Server
+        </v-btn>
     </v-form>
   </v-card>
 </template>
@@ -51,9 +69,15 @@ import { Component, Vue } from 'vue-property-decorator'
 import { Socket } from 'vue-socket.io-extended'
 import ClientSocketEvents from '@/models/ClientSocketEvents'
 import { HostOptions as HostOptionsModel } from '@/models/RoomModel'
+import ServerConnector from '@/components/ServerConnector.vue'
 
-@Component({})
+@Component({
+  components: {
+    ServerConnector
+  }
+})
 export default class HostOptions extends Vue {
+  dialog = false;
   updateOptions () {
     this.$socket.client.emit(
       ClientSocketEvents.SetOptions,
